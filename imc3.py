02e25fcf0a018850b139bac1a19db03d84ff18f8
kingderka123-spec <kingderka123@gmail.com>
2026-02-17 14:07:04 +0000

    Add files via upload

---
+++ imc3.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("imc")
+
+poids=st.number_input("veuillez donner votre poids")
+
+taille =st.number_input("veuillez donner votre taille")
+if st.button("calcul"):
+    imc=poids/(taille**2)
+
+    st.write(imc)
+
+if imc<18.5:
+    st.warning("maigre")
+elif 18.5<imc > 25:
+    
+    st.success("poids normal")
+elif 25<imc>30:
+    st.warning("surpoids")
+elif imc>30 :
+    st.warning("obése")
+else:
+    st.info("ceci n'est pas un nombre")
+    
